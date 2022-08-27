9825423ca9c19304b7d3ae80e050f225d281f858
Daniel Haas <daniel.c.haas@gmail.com>
2022-08-27 06:16:51 -0400

    Re-factored Ship factory to be initialized with a starting and ending position. Completed placeShip, receiveAttack and allSunk methods in the gameboard. Future TODO is to not let ships be overlapping.

--- battleship.js
+++ battleship.js
@@ -1,6 +1,12 @@
-const Ship = (_length) => {
-  let deck = Array(parseFloat(_length)).fill("");
-  let len = deck.length;
+const Ship = (start, end) => {
+  let len = Math.abs(start - end) + 1;
+  let deck = (() => {
+    let tmp = {};
+    for (let i = start; i <= end; i++) {
+      tmp[i] = "";
+    }
+    return tmp;
+  })();
 
   const hit = (pos) => {
     deck[pos] = "hit";
@@ -8,8 +14,8 @@ const Ship = (_length) => {
 
   const isSunk = () => {
     let sunk = true;
-    deck.forEach((pos) => {
-      if (pos !== "hit") {
+    Object.keys(deck).forEach((pos) => {
+      if (deck[pos] !== "hit") {
         sunk = false;
       }
     });
@@ -19,7 +25,57 @@ const Ship = (_length) => {
   return { hit, len, isSunk };
 };
 
-module.exports = { Ship };
+const Gameboard = () => {
+  let board = Array.from({ length: 10 }, () =>
+    Array.from({ length: 10 }, () => "")
+  );
 
-ship = Ship(5);
-console.log(ship);
+  const placeShip = (start, end) => {
+    let ship = {};
+    if (start[0] === end[0]) {
+      ship = Ship(start[1], end[1]);
+      for (let i = start[1]; i <= end[1]; i++) {
+        board[start[0]][i] = ship;
+      }
+    } else {
+      ship = Ship(start[0], end[0]);
+      for (let i = start[0]; i <= end[0]; i++) {
+        board[i][start[1]] = ship;
+      }
+    }
+    return ship;
+  };
+
+  const receiveAttack = (x, y) => {
+    if (typeof board[x][y] === "object") {
+      let tmp = board[x][y];
+      if (
+        (x < 9 && tmp == board[x + 1][y]) ||
+        (x > 0 && tmp == board[x - 1][y])
+      ) {
+        board[x][y].hit(x);
+      } else {
+        board[x][y].hit(y);
+      }
+    } else {
+      board[x][y] = "x";
+    }
+  };
+
+  const allSunk = () => {
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        if (typeof board[i][j] === "object") {
+          if (!board[i][j].isSunk()) {
+            return false;
+          }
+        }
+      }
+    }
+    return true;
+  };
+
+  return { board, placeShip, receiveAttack, allSunk };
+};
+
+module.exports = { Ship, Gameboard };
--- battleship.test.js
+++ battleship.test.js
@@ -1,20 +1,61 @@
-const { Ship } = require("./battleship.js");
+const { Ship, Gameboard } = require("./battleship.js");
 
 // beforeAll(() => {
 //   let ship = Ship();
 // });
 
 test("Test the ships hit and sunk function not sunk", () => {
-  let ship = Ship(3);
-  ship.hit(0);
-  ship.hit(1);
+  let ship = Ship(3, 7);
+  ship.hit(3);
+  ship.hit(4);
   expect(ship.isSunk()).toBe(false);
 });
 
 test("Test the ships hit and sunk function sunk", () => {
-  let ship = Ship(3);
+  let ship = Ship(0, 2);
   ship.hit(0);
   ship.hit(1);
   ship.hit(2);
   expect(ship.isSunk()).toBe(true);
+});
+
+test("Test the place ship functionality", () => {
+  let game = Gameboard();
+  let ship = game.placeShip([0, 0], [0, 3]);
+  expect(game.board[0][3]).toEqual(ship);
+});
+
+test("Test the receive attack functionality", () => {
+  let game = Gameboard();
+  let ship = game.placeShip([0, 0], [0, 3]);
+  game.receiveAttack(0, 0);
+  game.receiveAttack(0, 1);
+  game.receiveAttack(0, 2);
+  game.receiveAttack(0, 3);
+  expect(game.board[0][2].isSunk()).toBe(true);
+});
+
+test("Test the all sunk failed functionality", () => {
+  let game = Gameboard();
+  game.placeShip([0, 0], [0, 3]);
+  game.placeShip([1, 0], [1, 3]);
+  game.receiveAttack(0, 0);
+  game.receiveAttack(0, 1);
+  game.receiveAttack(0, 2);
+  game.receiveAttack(0, 3);
+  expect(game.allSunk()).toBe(false);
+});
+
+test("Test the all sunk true functionality", () => {
+  let game = Gameboard();
+  game.placeShip([0, 0], [0, 3]);
+  game.placeShip([1, 0], [1, 2]);
+  game.receiveAttack(0, 0);
+  game.receiveAttack(0, 1);
+  game.receiveAttack(0, 2);
+  game.receiveAttack(0, 3);
+  game.receiveAttack(1, 0);
+  game.receiveAttack(1, 1);
+  game.receiveAttack(1, 2);
+  expect(game.allSunk()).toBe(true);
 });
